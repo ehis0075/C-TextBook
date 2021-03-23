a16a9ada85a726590e33089b2e8b12e74c8c7195
Ehis <ehisjude420@gmail.com>
2021-03-23 01:00:45 +0100

    indexer

---
+++ C#book/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace C_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private float[] weekTemp = { 47.9F, 34.8F, 12.0F, 26.3F};
+
+            //use float indexer, to encapsulate weekTemp
+            public float this[int index]
+            {
+                get
+                {
+                    if(index >= 0 && < weekTemp.Length)
+                    {
+                        return weekTemp[index];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                Set
+                {
+                    if(value > 0)
+                    {
+                        weekTemp[index] = value;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("pls set value greater than 0");
+                    }
+                }
+            }
+        }
+    }
+}
